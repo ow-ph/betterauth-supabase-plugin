e421c69693269cbeb5dd0c95ccec2e696a0d0df7
PH <ph@squadfy.com.br>
2025-06-23 13:07:17 -0300

    Update tsup.config.ts
    use the correct client.ts file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['index.ts', 'client-plugin.ts'],
+  entry: ['index.ts', 'client.ts'],
   format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
